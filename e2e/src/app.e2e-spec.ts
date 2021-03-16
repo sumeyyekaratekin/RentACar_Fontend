6fcf95ee961f3d2eb02364c42ff7830d90b45a7c
sumeyyekaratekin <sumeyyekaratekin96@gmail.com>
2021-03-16 13:32:11 +0300

    Some Files Updated.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('re-cap-project-frontend app is running!');
+    expect(await page.getTitleText()).toEqual('carRentalFrontend app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/re-cap-project-frontend'),
+      dir: require('path').join(__dirname, './coverage/carRentalFrontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
