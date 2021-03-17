32c03597935b73cf8fbc25d4b7ebe07a07faec7a
sumeyyekaratekin <sumeyyekaratekin96@gmail.com>
2021-03-18 00:45:00 +0300

    Updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms"
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
